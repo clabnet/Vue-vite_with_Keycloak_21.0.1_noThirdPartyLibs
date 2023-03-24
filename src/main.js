fb3dedeaac6da259ba083418e57a9d09849445e8
Claudio Barca <c.barca@libero.it>
2023-03-24 18:26:17 +0100

    refactoring

--- src/main.js
+++ src/main.js
@@ -6,22 +6,6 @@ import VueAxios from "vue-axios";
 import { h, reactive } from "vue";
 
 //==== Keycloack ====================== 
-// https://www.keycloak.org/docs/latest/securing_apps/index.html#_javascript_adapter
-// let initOptions = {
-//   // realm: "demo",
-//   // url: "http://localhost:8080/",
-//   // clientId: "app",
-//   "onLoad": "login-required",
-//   "clientId": "app",
-//   "realm": "demo",
-//   "auth-server-url": "http://localhost:8080/realms/demo/protocol/openid-connect/auth/",
-//   // "ssl-required": "external",
-//   // "resource": "app",
-//   // "public-client": true,
-//   // "confidential-port": 0
-// };
-
-
 let initOptions = {
   url: 'http://localhost:8080/', realm: 'demo', clientId: 'app', onLoad: 'login-required'
 }
